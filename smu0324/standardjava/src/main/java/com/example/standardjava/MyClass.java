908fcd900dd8347ddaad98db1df134948cc0425f
J-E shine <amy5549@naver.com>
2021-03-24 09:30:45 +0900

    committing.

---
+++ smu0324/standardjava/src/main/java/com/example/standardjava/MyClass.java
@@ -0,0 +1,9 @@
+package com.example.standardjava;
+
+public class MyClass {
+    public static void main(String[]args){
+        String hi = "hello, my first std. JAVA programs!";
+        System.out.println(hi);
+    }
+    }
+
